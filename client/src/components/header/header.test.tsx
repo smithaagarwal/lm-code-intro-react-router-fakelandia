cc5b7693340c6eb3cc70339f21d08214af3d42c5
smithaagarwal <smitha.kotian@gmail.com>
2023-12-31 01:04:05 +0000

    feat:add testcases for header component

---
+++ client/src/components/header/header.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import { describe } from "node:test";
+import Header from "./header";
+import { BrowserRouter } from "react-router-dom";
+
+describe("Header component", () => {
+  test("When the component is rendered, then the header text should be present", () => {
+    render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    );
+    const someText = screen.getByText("Fakelandia Justice Department");
+    expect(someText).toBeInTheDocument();
+  });
+});
