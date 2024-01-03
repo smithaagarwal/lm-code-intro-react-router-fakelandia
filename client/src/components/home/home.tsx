22af3764f8e7c4c62e51d5c4dc78fcdd886af3e0
smithaagarwal <smitha.kotian@gmail.com>
2024-01-03 23:21:17 +0000

    feat: add logic to display total number of misdemeanours

--- client/src/components/home/home.tsx
+++ client/src/components/home/home.tsx
@@ -1,11 +1,16 @@
-const Home: React.FC = () => (
-  <section className="content">
-    <p>Welcome to the home of the Justice Department of Fakelandia</p>
-    <p>
-      Here you can browse a list of recent misdemeanours committed by our
-      citizens, or you can confess to your own misdemeanours.
-    </p>
-  </section>
-);
-
+import { useContext } from "react";
+import { ParentDataContext } from "../parent_data_provider";
+const Home: React.FC = () => {
+  const parentData = useContext(ParentDataContext);
+  return (
+    <section className="content">
+      <p>Welcome to the home of the Justice Department of Fakelandia</p>
+      <p>
+        Here you can browse a list of recent misdemeanours committed by our
+        citizens, or you can confess to your own misdemeanours.
+      </p>
+      <h2>Total misdemeanours is {parentData.misdemeanours.length}</h2>
+    </section>
+  );
+};
 export default Home;
